d9364e6995ddca8b1b0fb19b4793998a7d5d40d0
Mohammad Sadeghi <sadeghi85@hotmail.com>
2014-09-10 18:54:13 +0430

    .

--- cmd/domaindef.sh
+++ cmd/domaindef.sh
@@ -23,17 +23,17 @@ SERVER_TAG=$(echo "$1" | tr '[A-Z]' '[a-z]')
 SERVER_NAME=$(echo "$2" | tr '[A-Z]' '[a-z]')
 SERVER_PORT=$(echo "$3" | tr '[A-Z]' '[a-z]')
 
-if [[ ! (echo "$SERVER_TAG" | grep -Pqs "^web\d{3}$" ]]; then
+if ! $(echo "$SERVER_TAG" | grep -Pqs "^web\d{3}$"); then
 	echo "SERVER_TAG ($SERVER_TAG) is invalid."
 	exit 1
 fi
 
-if [[ ! (echo "$SERVER_NAME" | grep -Pqs "^([a-z0-9][-a-z0-9]*\.)+[a-z]+$") ]]; then
+if ! $(echo "$SERVER_NAME" | grep -Pqs "^([a-z0-9][-a-z0-9]*\.)+[a-z]+$"); then
 	echo "SERVER_NAME ($SERVER_NAME) is invalid."
 	exit 1
 fi
 
-if [[ ! (echo "$SERVER_PORT" | grep -Pqs "^\d+$") ]]; then
+if ! $(echo "$SERVER_PORT" | grep -Pqs "^\d+$"); then
 	echo "SERVER_PORT ($SERVER_PORT) is invalid."
 	exit 1
 fi
@@ -49,6 +49,16 @@ if [[ ! -e "$HOME/sites-available/$SERVER_TAG" ]]; then
 		exit 1
 	fi
 
+	STATUS=$(ln -fs "../sites-available/$SERVER_TAG/" "$HOME/sites-enabled/$SERVER_TAG" 2>&1)
+	
+	if (( $? != 0 )); then
+		echo "$STATUS"
+		exit 1
+	fi
+	
+	STATUS=$(ln -fs "../sites-available/$SERVER_TAG/" "$HOME/sites-available-for-humans/$SERVER_PORT.$SERVER_NAME" 2>&1)
+	STATUS=$(ln -fs "../sites-available/$SERVER_TAG/" "$HOME/sites-enabled-for-humans/$SERVER_PORT.$SERVER_NAME" 2>&1)
+	
 	# creating web root
 	STATUS=$(mkdir -p "$HOME/sites-available/$SERVER_TAG/$WEB_ROOT_DIR" 2>&1)
 	
@@ -62,7 +72,7 @@ if [[ ! -e "$HOME/sites-available/$SERVER_TAG" ]]; then
 	
 	if (( $? == 0 )); then
 		# user exists
-		STATUS=$(usermod "$SERVER_TAG" --home "$HOME/sites-available/$SERVER_TAG" --shell "$SHELL" 2>&1)
+		STATUS=$(usermod --comment "$SERVER_NAME $SERVER_PORT" -g apache --home "$HOME/sites-available/$SERVER_TAG" --shell "$SHELL" "$SERVER_TAG" 2>&1)
 		
 		if (( $? != 0 )); then
 			echo "$STATUS"
@@ -70,7 +80,7 @@ if [[ ! -e "$HOME/sites-available/$SERVER_TAG" ]]; then
 		fi
 	else
 		# user doesn't exist
-		STATUS=$(useradd "$SERVER_TAG" --home "$HOME/sites-available/$SERVER_TAG" --shell "$SHELL" 2>&1)
+		STATUS=$(useradd --comment "$SERVER_NAME $SERVER_PORT" -g apache --home "$HOME/sites-available/$SERVER_TAG" --shell "$SHELL" "$SERVER_TAG" 2>&1)
 		
 		if (( $? != 0 )); then
 			echo "$STATUS"
@@ -79,158 +89,159 @@ if [[ ! -e "$HOME/sites-available/$SERVER_TAG" ]]; then
 	fi
 
 	# correcting permissions on ftp home
-	echo "Correcting permissions on ftp home..."
-	STATUS=$(chown -R "u-$SAFE_SERVER_NAME:u-$SAFE_SERVER_NAME" "$HOME/$SERVER_NAME" 2>&1)
+	# STATUS=$(chown -R "$SERVER_TAG:apache" "$HOME/sites-available/$SERVER_TAG" 2>&1)
+	# STATUS=$(chmod -R 644 "$HOME/sites-available/$SERVER_TAG" 2>&1)
+	# STATUS=$(chmod -R +X "$HOME/sites-available/$SERVER_TAG" 2>&1) # to give search bit to all directories, effectively 755 for dirs
 	
-	if [ $? != 0 ]; then
-		echo "Couldn't set permissions on ftp home. Aborting.... message:($STATUS)"
-		exit 1
-	fi
-
-	STATUS=$(chmod -R 644 "$HOME/$SERVER_NAME" 2>&1)
-	
-	if [ $? != 0 ]; then
-		echo "Couldn't set permissions on ftp home. Aborting.... message:($STATUS)"
-		exit 1
-	fi
-
-	STATUS=$(chmod -R +X "$HOME/$SERVER_NAME" 2>&1) # to give search bit to all directories, effectively 755 for dirs
-	
-	if [ $? != 0 ]; then
-		echo "Couldn't set permissions on ftp home. Aborting.... message:($STATUS)"
-		exit 1
-	fi
-
-	# creating pool definition
-	echo "Creating pool definition..."
-	if [ ! -e "/etc/php-fpm.d/settings/sites-available/$SERVER_NAME.conf" ]; then
-		STATUS=$(cp "$SCRIPT_DIR/templates/php-fpm/example.com.conf" "/etc/php-fpm.d/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+	# creating PHP-FPM pool definition
+	if [[ ! -e "/etc/php-fpm.d/settings/sites-available/$SERVER_TAG.conf" ]]; then
+		# config file doesn't exist
+		STATUS=$(cp "$SCRIPT_DIR/templates/php-fpm/web001.conf" "/etc/php-fpm.d/settings/sites-available/$SERVER_TAG.conf" 2>&1)
 		
-		if [ $? != 0 ]; then
-			echo "Couldn't copy the template. Aborting.... message:($STATUS)"
+		if (( $? != 0 )); then
+			echo "$STATUS"
 			exit 1
 		else
-			STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" -e"s/^user\s\+=.*/user = u-$SAFE_SERVER_NAME/" -e"s/^group\s\+=.*/group = u-$SAFE_SERVER_NAME/" "/etc/php-fpm.d/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+			STATUS=$(sed -i -e"s/web001/$SERVER_TAG/g" "/etc/php-fpm.d/settings/sites-available/$SERVER_TAG.conf" 2>&1)
 			
-			if [ $? != 0 ]; then
-				echo "Couldn't edit the pool definition. Aborting.... message:($STATUS)"
+			if (( $? != 0 )); then
+				echo "$STATUS"
 				exit 1
 			else
-				STATUS=$(ln -fs "../sites-available/$SERVER_NAME.conf" "/etc/php-fpm.d/settings/sites-enabled/$SERVER_NAME.conf" 2>&1)
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/php-fpm.d/settings/sites-enabled/$SERVER_TAG.conf" 2>&1)
 			
-				if [ $? != 0 ]; then
-					echo "Couldn't enable the pool. Aborting.... message:($STATUS)"
+				if (( $? != 0 )); then
+					echo "$STATUS"
 					exit 1
 				fi
+				
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/php-fpm.d/settings/sites-available-for-humans/$SERVER_PORT.$SERVER_NAME.conf" 2>&1)
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/php-fpm.d/settings/sites-enabled-for-humans/$SERVER_PORT.$SERVER_NAME.conf" 2>&1)
 			fi
 		fi
 	else
-		echo "WARNING: Pool definition already exists."
+		# config file exists
+		echo ""
 	fi
 
 	# creating apache virtual host
-	echo "Creating apache virtual host..."
-	if [ ! -e "/etc/httpd/settings/sites-available/$SERVER_NAME.conf" ]; then
-		STATUS=$(cp "$SCRIPT_DIR/templates/apache/example.com.conf" "/etc/httpd/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+	if [ ! -e "/etc/httpd/settings/sites-available/$SERVER_TAG.conf" ]; then
+		# config file doesn't exist
+		STATUS=$(cp "$SCRIPT_DIR/templates/apache/web001.conf" "/etc/httpd/settings/sites-available/$SERVER_TAG.conf" 2>&1)
 		
-		if [ $? != 0 ]; then
-			echo "Couldn't copy the virtual host template. Aborting.... message:($STATUS)"
+		if (( $? != 0 )); then
+			echo "$STATUS"
 			exit 1
 		else
-			STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" "/etc/httpd/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+			STATUS=$(sed -i -e"s/web001/$SERVER_TAG/g" -e"s/ServerName .*/ServerName $SERVER_PORT.$SERVER_NAME:$SERVER_PORT/" -e"s/ServerAlias .*/ServerAlias $SERVER_PORT.www.$SERVER_NAME:$SERVER_PORT/" -e"s/ServerAdmin .*/ServerAdmin postmaster@$SERVER_NAME/" "/etc/httpd/settings/sites-available/$SERVER_TAG.conf" 2>&1)
 			
-			if [ $? != 0 ]; then
-				echo "Couldn't edit the virtual host. Aborting.... message:($STATUS)"
+			if (( $? != 0 )); then
+				echo "$STATUS"
 				exit 1
 			else
-				STATUS=$(ln -fs "../sites-available/$SERVER_NAME.conf" "/etc/httpd/settings/sites-enabled/$SERVER_NAME.conf" 2>&1)
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/httpd/settings/sites-enabled/$SERVER_TAG.conf" 2>&1)
 			
-				if [ $? != 0 ]; then
-					echo "Couldn't enable the virtual host. Aborting.... message:($STATUS)"
+				if (( $? != 0 )); then
+					echo "$STATUS"
 					exit 1
 				fi
+				
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/httpd/settings/sites-available-for-humans/$SERVER_PORT.$SERVER_NAME.conf" 2>&1)
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/httpd/settings/sites-enabled-for-humans/$SERVER_PORT.$SERVER_NAME.conf" 2>&1)
 			fi
 		fi
 	else
-		echo "WARNING: Apache virtual host already exists."
+		# config file exists
+		echo ""
 	fi
 
 	# creating nginx virtual host
-	echo "Creating nginx virtual host..."
-	if [ ! -e "/etc/nginx/settings/sites-available/$SERVER_NAME.conf" ]; then
-		STATUS=$(cp "$SCRIPT_DIR/templates/nginx/example.com.conf" "/etc/nginx/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+	if [ ! -e "/etc/nginx/settings/sites-available/$SERVER_TAG.conf" ]; then
+		# config file doesn't exist
+		STATUS=$(cp "$SCRIPT_DIR/templates/nginx/web001.conf" "/etc/nginx/settings/sites-available/$SERVER_TAG.conf" 2>&1)
 		
-		if [ $? != 0 ]; then
-			echo "Couldn't copy the virtual host template. Aborting.... message:($STATUS)"
+		if (( $? != 0 )); then
+			echo "$STATUS"
 			exit 1
 		else
-			STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" -e"s/\(listen\s\+\).*/\1$IP_PORT;/" "/etc/nginx/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+			STATUS=$(sed -i -e"s/web001/$SERVER_TAG/g" -e"s/server_name .*/server_name $SERVER_NAME www.$SERVER_NAME;/" -e"s/listen .*/listen $SERVER_PORT;/" "/etc/nginx/settings/sites-available/$SERVER_TAG.conf" 2>&1)
 			
-			if [ $? != 0 ]; then
-				echo "Couldn't edit the virtual host. Aborting.... message:($STATUS)"
+			if (( $? != 0 )); then
+				echo "$STATUS"
 				exit 1
 			else
-				STATUS=$(ln -fs "../sites-available/$SERVER_NAME.conf" "/etc/nginx/settings/sites-enabled/$SERVER_NAME.conf" 2>&1)
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/nginx/settings/sites-enabled/$SERVER_TAG.conf" 2>&1)
 			
-				if [ $? != 0 ]; then
-					echo "Couldn't enable the virtual host. Aborting.... message:($STATUS)"
+				if (( $? != 0 )); then
+					echo "$STATUS"
 					exit 1
 				fi
+				
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/nginx/settings/sites-available-for-humans/$SERVER_PORT.$SERVER_NAME.conf" 2>&1)
+				STATUS=$(ln -fs "../sites-available/$SERVER_TAG.conf" "/etc/nginx/settings/sites-enabled-for-humans/$SERVER_PORT.$SERVER_NAME.conf" 2>&1)
 			fi
+			
+			if ! $(grep -Pqs "listen\s+$SERVER_PORT" "/etc/nginx/nginx_default_server.conf"); then
+				if (( $SERVER_PORT != 80 )); then
+					STATUS=$(sed -i -e"s/\(listen .*80 .*default_server;\)/\1 listen $SERVER_PORT default_server;/" "/etc/nginx/nginx_default_server.conf" 2>&1)
+				fi
+			fi
+			
 		fi
 	else
-		echo "WARNING: Nginx virtual host already exists."
+		# config file exists
+		echo ""
 	fi
 
 	# copying default index page
-	echo "Copying default index page..."
-	STATUS=$(cp "$SCRIPT_DIR/templates/web/index.php" "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
+	# echo "Copying default index page..."
+	# STATUS=$(cp "$SCRIPT_DIR/templates/web/index.php" "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
 	
-	if [ $? != 0 ]; then
-		echo "WARNING: Couldn't copy the default index page. message:($STATUS)"
-	else
-		STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
-		STATUS=$(chown "u-$SAFE_SERVER_NAME:u-$SAFE_SERVER_NAME" "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
-		STATUS=$(chmod 644 "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
-	fi
+	# if (( $? != 0 )); then
+		# echo "WARNING: Couldn't copy the default index page. message:($STATUS)"
+	# else
+		# STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
+		# STATUS=$(chown "u-$SAFE_SERVER_NAME:u-$SAFE_SERVER_NAME" "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
+		# STATUS=$(chmod 644 "$HOME/$SERVER_NAME/$WEB_ROOT_DIR/index.php" 2>&1)
+	# fi
 	
 	# creating webalizer config
-	echo "Creating webalizer config..."
-	if [ ! -e "/etc/webalizer.d/sites-available/$SERVER_NAME.conf" ]; then
-		STATUS=$(cp "$SCRIPT_DIR/templates/webalizer/example.com.conf" "/etc/webalizer.d/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+	# echo "Creating webalizer config..."
+	# if [ ! -e "/etc/webalizer.d/sites-available/$SERVER_NAME.conf" ]; then
+		# STATUS=$(cp "$SCRIPT_DIR/templates/webalizer/example.com.conf" "/etc/webalizer.d/settings/sites-available/$SERVER_NAME.conf" 2>&1)
 	
-		if [ $? != 0 ]; then
-			echo "WARNING: Couldn't copy the webalizer template. message:($STATUS)"
-		else
-			STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" "/etc/webalizer.d/settings/sites-available/$SERVER_NAME.conf" 2>&1)
+		# if (( $? != 0 )); then
+			# echo "WARNING: Couldn't copy the webalizer template. message:($STATUS)"
+		# else
+			# STATUS=$(sed -i -e"s/example\.com/$SERVER_NAME/g" "/etc/webalizer.d/settings/sites-available/$SERVER_NAME.conf" 2>&1)
 			
-			if [ $? != 0 ]; then
-				echo "WARNING: Couldn't edit the webalizer config. message:($STATUS)"
-			else
-				STATUS=$(ln -fs "../sites-available/$SERVER_NAME.conf" "/etc/webalizer.d/settings/sites-enabled/$SERVER_NAME.conf" 2>&1)
+			# if (( $? != 0 )); then
+				# echo "WARNING: Couldn't edit the webalizer config. message:($STATUS)"
+			# else
+				# STATUS=$(ln -fs "../sites-available/$SERVER_NAME.conf" "/etc/webalizer.d/settings/sites-enabled/$SERVER_NAME.conf" 2>&1)
 			
-				if [ $? != 0 ]; then
-					echo "Couldn't enable the webalizer config. message:($STATUS)"
-				fi
-			fi
-		fi
-	else
-		echo "WARNING: Webalizer config already exists."
-	fi
+				# if (( $? != 0 )); then
+					# echo "Couldn't enable the webalizer config. message:($STATUS)"
+				# fi
+			# fi
+		# fi
+	# else
+		# echo "WARNING: Webalizer config already exists."
+	# fi
 
 	# Restarting servers
-	echo "Restarting servers..."
-	STATUS=$(sh "$SCRIPT_DIR/restart_servers.sh" 2>&1)
+	# echo "Restarting servers..."
+	# STATUS=$(sh "$SCRIPT_DIR/restart_servers.sh" 2>&1)
 	
-	if [ $? != 0 ]; then
-		echo -e "$STATUS\nRestart failed..."
-		exit 1
-	else
-		echo "$STATUS"
-	fi
+	# if (( $? != 0 )); then
+		# echo -e "$STATUS\nRestart failed..."
+		# exit 1
+	# else
+		# echo "$STATUS"
+	# fi
 
 else
-	echo "Directory ($HOME/$SERVER_TAG) already exists."
+	echo "Directory ($HOME/sites-available/$SERVER_TAG) already exists."
 	exit 1
 fi
 
