8cb43c62427a2617d09be9f361267989796ab7ca
Mohammad Sadeghi <sadeghi85@hotmail.com>
2014-09-12 22:16:54 +0430

    .

--- cmd/userquota.sh
+++ cmd/userquota.sh
@@ -1,64 +1,58 @@
 #!/bin/bash
 
-# Check for a valid number of parameters or for the help command
-if [ "$1" == '--help' -o "$#" != 2 ]; then
-	printf "The usage for this command is:\n"
-	printf "\tuserquota.sh '\e[33msite_name\e[0m' '\e[32mquota_size\e[0m'\n"
-	printf "Where the variables are:\n"
-	printf "\t\e[33msite_name\e[0m\tThe name of the site\n"
-	printf "\t\e[32mquota_size\e[0m\tThe quota size in KB\n"
-	exit 0
-fi
-
-# Allow only root execution
-if [ $(id -u) -ne 0 ]; then
-    echo "This script requires root privileges"
-    exit 1
-fi
+# $1:server_tag, $2:quota_size (KB)
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 HOME="/var/www/WebPanel"
 
-SERVER_TAG="$(echo $1 | tr '[A-Z]' '[a-z]')"
-
-if ! (echo "$SERVER_NAME" | grep -Eq "^([a-z0-9][-a-z0-9]*\.)*[a-z]*$") then
-	echo "SERVER_NAME ($SERVER_NAME) doesn't seem to be valid. Aborting...."
+HOME=${HOME%/}
+if [[ $HOME == "" ]]; then
+	echo "Home directory can't be '/' itself."
 	exit 1
 fi
 
-SAFE_SERVER_NAME="${SERVER_NAME//./_}"
-SAFE_SERVER_NAME="$(echo $SAFE_SERVER_NAME | cut -c1-30)" # truncate to 30 chars, so after adding u- it doesn't go beyond 32 char limit.
+# Allow only root execution
+if (( $(id -u) != 0 )); then
+    echo "This script requires root privileges"
+    exit 1
+fi
 
-QUOTASIZE="$2"
+SERVER_TAG=$(echo "$1" | tr '[A-Z]' '[a-z]')
+QUOTA_SIZE="$2"
 
-if ! (echo "$QUOTASIZE" | grep -Pq "^\d+$") then
-	echo "QUOTASIZE ($QUOTASIZE) doesn't seem to be valid. Aborting...."
+if ! $(echo "$SERVER_TAG" | grep -Pqs "^web\d{3}$"); then
+	echo "SERVER_TAG ($SERVER_TAG) is invalid."
+	exit 1
+fi
+
+if ! $(echo "$QUOTA_SIZE" | grep -Pqs "^\d+$"); then
+	echo "QUOTA_SIZE ($QUOTA_SIZE) is invalid."
 	exit 1
 fi
 
 # Quota
 TMP_DIR="/$HOME"
 QUOTA_DIR=""
-while [ "$TMP_DIR" != "" ]
+while [[ $TMP_DIR != "" ]]
 do
-	if [ "$TMP_DIR" != "/" ]; then
+	if [[ $TMP_DIR != "/" ]]; then
 		QUOTA_DIR=$(echo "$TMP_DIR" | sed -r -e"s/^\/(.+)$/\1/");
 	else
 		QUOTA_DIR="/"
 	fi
 	
-	if grep -qs " $QUOTA_DIR " "/proc/mounts"; then
+	if grep -qs " $QUOTA_DIR " /proc/mounts; then
 		break
 	fi
 	
 	TMP_DIR=$(echo "$TMP_DIR" | sed -r -e"s/^(.*)\/.*?$/\1/")
 done
 
-echo "Setting quota for user: u-$SAFE_SERVER_NAME"
-STATUS=$(setquota -u "u-$SAFE_SERVER_NAME" -F vfsv0 0 "$QUOTASIZE" 0 0 "$QUOTA_DIR" -a 2>&1)
+# setting quota size
+STATUS=$(setquota -u "$SERVER_TAG" -F vfsv0 0 "$QUOTA_SIZE" 0 0 "$QUOTA_DIR" -a 2>&1)
 
-if [ $? != 0 ]; then
-	echo "Couldn't set the quota. Aborting.... message:($STATUS)"
+if (( $? != 0 )); then
+	echo "$STATUS"
 	exit 1
 fi
 
